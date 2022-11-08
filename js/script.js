b892d502496e82ff39bcbcaee30083f04c7f4b43
StefaniaSperani <thedarkcry@hotmail.it>
2022-11-08 16:26:46 +0100

    comments and better css

--- js/script.js
+++ js/script.js
@@ -7,29 +7,37 @@ Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 */
 
 
-const {createApp} = Vue;
+const { createApp } = Vue;
 // Creo l'app
 const app = createApp({
     // inserisco i data
-    data(){
+    data() {
         return {
+            //setto la mia lista delle email vuota
             emailList: [],
         }
     },
     //creo i methods, dove inserisco le funzioni
     methods: {
-        callApi(){
-            axios.get('https:flynn.boolean.careers/exercises/api/random/mail').then((res)=>{
-                this.emailList.push(res.data.response);
-                
-            })
-            console.log(this.emailList);
-        },
-    },
-    created(){
-        for(let i = 0; i < 10; i++){
-            this.callApi();
+        //creo la funzione per chiamare lapi con la lista delle mail
+        callApi() {
+            //creo un ciclo che mi stamperà 10 email
+            for (let i = 0; i < 10; i++) {
+                //tramite axios prendo l'api boolean con le random mail
+                axios.get('https:flynn.boolean.careers/exercises/api/random/mail').then((res) => {
+                    //e una volta ciclate le pusho al suo intenro
+                    this.emailList.push(res.data.response);
+                })
+            }
+            //avendo aggiunt un button per generarne di nuove, ripulisco
+            //in modo da avere sempre 10 email
+            this.emailList = [];
         }
+    },
+    //monto la funzione anche nella created, così appena 
+    //apro la pagina la lista esce subito
+    created() {
+        this.callApi();
     }
-//monto tutto sul div #app
+    //monto tutto sul div #app
 }).mount('#app')
