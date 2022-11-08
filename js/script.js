ce0b3dae961011a3c203cadadc33a83f89aca426
StefaniaSperani <thedarkcry@hotmail.it>
2022-11-08 14:39:57 +0100

    documents

---
+++ js/script.js
@@ -0,0 +1,23 @@
+/*
+Attraverso l'apposita API di Boolean :faccia_che_festeggia:
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+*/
+
+
+const {createApp} = Vue;
+// Creo l'app
+const app = createApp({
+    // inserisco i data
+    data(){
+        return {
+            
+        }
+    },
+    //creo i methods, dove inserisco le funzioni
+    methods: {
+    }
+//monto tutto sul div #app
+}).mount('#app')
